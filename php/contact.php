b9bac9d2dcd95800ead43f00a3973bef1d0eff47
dolorierproject <dolorierproject@gmail.com>
2012-07-09 00:54:17 -0500

    contacto

--- php/contact.php
+++ php/contact.php
@@ -31,8 +31,9 @@ class Contact{
 		
 		$email		= $_POST['email'];
 		$name		= $_POST['name'];
+		$phone		= $_POST['mobile'];
 		$website	= $_POST['website'];
-		$message	= $_POST['message'];
+		$message	= $_POST['message'];		
 		
 		/* Server Side Data Validation */
 		
@@ -72,6 +73,7 @@ class Contact{
 				'errors' => array(
 								array('name' => 'email'		,'value' => $this->error_value('email')),
 								array('name' => 'name' 		,'value' => $this->error_value('name')),
+								array('name' => 'mobile' 	,'value' => $this->error_value('mobile')),
 								array('name' => 'website'	,'value' => $this->error_value('website')),
 								array('name' => 'message'	,'value' => $this->error_value('message'))
 							)
@@ -99,14 +101,63 @@ class Contact{
 	
 	public function sendEmail($email,$name,$website,$message){
 		/* Just format the email text the way you want ... */
-		$message_body		= "Hi, ".$name."(".$email." - ".$website.") sent you a message from yoursite.com\n"
+		/*$message_body		= "Hola, ".$name."(".$email." - ".$website.") sent you a message from yoursite.com\n"
 									."email: ".$email."\n"
 									."message: "."\n"
-									.$message; 
+									.$message; */
+
 		//$headers			= "From: ".EMAIL_FROM_NAME." <".EMAIL_FROM_ADDR.">";
-		$to = "pdolorier@hotmail.com";
+		$message_body		=
+'<style>
+table, td{
+	font: 14px/130% Arial, Helvetica, sans-serif;
+	width:500px;		
+	text-align: justify;
+}
+table th{
+	color:#a62300; 
+}
+table td.name{	
+	width:15%;
+	color: #025167;	
+}
+table td.get{
+	width:85%;		 
+	color: #025167;	
+}
+</style>
+<table cellpadding="4" cellspacing="0"  border="0" height:>
+	<thead>
+		<tr>
+			<th colspan="2" align="center">Nuevo Mensaje</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="name" align="left" valign="top">Nombre:</td>
+			<td class="get">'.$name.'</td>
+		</tr>
+		<tr>
+			<td class="name" align="left" valign="top">Email</td>
+			<td class="get">'.$email.'</td>
+		</tr>
+		<tr>
+			<td class="name" align="left" valign="top">Telefono:</td>
+			<td class="get">'.$mobile.'</td>
+		</tr>
+		<tr>
+			<td class="name" align="left" valign="top">Website:</td>
+			<td class="get" ><a href="http://'.$website.'" target="_blank">'.$website.'</a></td>
+		</tr>
+		<tr>
+			<td class="name" align="left" valign="top">Mensaje:</td>
+			<td class="get">'.$message.'</td>
+		</tr>
+	</tbody>
+</table>';
+		$to = "themastermiguel@hotmail.com,kev3280@hotmail.com,jimmyasra@hotmail.com,ppardoz@hotmail.com,pdolorier@hotmail.com";
 		$subject = "Test mail";
-		$headers = "From:";
+		$headers = "Content-Type: text/html;charset=utf-8";
 		//return mail(EMAIL_TO,MESSAGE_SUBJECT,$message_body,$headers);
 		return mail($to,$subject,$message_body,$headers);
 	}
